4525e9562b4acf21c0530a9bdaf412c5f88aabe9
NihadTk <81925773+ShunKaidou@users.noreply.github.com>
2025-04-02 11:30:38 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+//Hello
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
